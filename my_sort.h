0713242fdd605f715db2bcaf019ecb2d3e853b42
spite897 <89777527@qq.com>
2024-07-02 20:40:05 +0800

    Delete my_sort.h

--- my_sort.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string.h>
-void my_bsort(void* base, size_t nitems, size_t size, int (*compar)(const void*, const void*));
-
-void my_csort(void* base, size_t nitems, size_t size, int (*compar)(const void*, const void*));
